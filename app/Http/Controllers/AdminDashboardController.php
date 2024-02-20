727ec0c9e8056e57b74543007555a882195f5f33
zelphyx <rizqy.astiko19@gmail.com>
2024-02-20 08:16:05 +0700

    fix files and codes

--- app/Http/Controllers/AdminDashboardController.php
+++ app/Http/Controllers/AdminDashboardController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 class AdminDashboardController extends Controller
 {
     public function index(){
-        return view('layouts.dashboardmain',[
+        return view('dashboard.index',[
         ]);
     }
 }
--- resources/views/layouts/dashboardmain.blade.php
+++ resources/views/layouts/dashboardmain.blade.php
@@ -59,7 +59,7 @@
                             </a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link d-flex align-items-center gap-2" href="#">
+                            <a class="nav-link d-flex align-items-center gap-2" href="/dashboard/class/all">
                                 <i class="bi bi-building-fill"></i>
                                 Classes
                             </a>
